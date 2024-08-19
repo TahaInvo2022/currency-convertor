09d3592cb73e2f753618404541ede294b06aff12
TahaInvo2022 <taha.shahid@invozone.dev>
2024-08-19 12:34:54 +0500

    currency convertor self learning project

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
+    )
+      .then((response) => response.json())
+      .then((res) => setData(res[currency]))
+      .catch((error) => {
+        console.log("🚀 ~ useEffect ~ error:", error);
+      });
+  }, [currency]);
+console.log("🚀 ~ useCurrencyInfo ~ data:", data);
+  return data;
+}
+
+export default useCurrencyInfo
